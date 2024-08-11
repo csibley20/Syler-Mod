b7a692e2fd17dbbd79ca2ebc45a60b318c0ae769
csibley20 <connor.sibley@comcast.net>
2024-08-10 23:13:21 -0400

    Initial commit

---
+++ src/main/java/net/sylver/sylermod/SylerModClient.java
@@ -0,0 +1,10 @@
+package net.sylver.sylermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SylerModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sylver/sylermod/SylerModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sylver.sylermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SylerModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
